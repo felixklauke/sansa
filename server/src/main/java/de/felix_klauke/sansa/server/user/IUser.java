1b849eaf92be492027df29d988172367e67fefe5
Felix Klauke <info@felix-klauke.de>
2017-08-03 18:54:43 +0200

    Add docs for the user.

--- server/src/main/java/de/felix_klauke/sansa/server/user/IUser.java
+++ server/src/main/java/de/felix_klauke/sansa/server/user/IUser.java
@@ -2,7 +2,18 @@ package de.felix_klauke.sansa.server.user;
 
 public interface IUser {
 
+    /**
+     * Get the name of the user to authenticate with.
+     *
+     * @return The user name.
+     */
     String getUserName();
 
+    /**
+     * Check if the user can be authenticated with the given secret.
+     *
+     * @param password The secret.
+     * @return If the user could be authenticated.
+     */
     boolean checkPassword(String password);
 }
